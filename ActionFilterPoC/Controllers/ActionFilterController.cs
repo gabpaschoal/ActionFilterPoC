af30fb9ffc615302675e2aed097b8c5d523623fe
Guilherme Paschoal <gabpaschoal@outlook.com>
2021-11-07 09:53:04 -0300

    Add project files.

---
+++ ActionFilterPoC/Controllers/ActionFilterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionFilterPoC.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ActionFilterController : ControllerBase
+{
+    // GET: api/<ActionFilterController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+}
---
+++ ActionFilterPoC/Filters/ActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilterPoC.Filters;
+
+public class ActionFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    { }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        context.HttpContext.Response.StatusCode = 999;
+        var newResult = context.Result as ObjectResult;
+        if (newResult is not null)
+            newResult.Value = "Modified by filter";
+    }
+}
